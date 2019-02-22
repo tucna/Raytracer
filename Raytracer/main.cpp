dee93c8bf615ad4f640f831169f4fb0f3bfe2d9c
Pavel Vlasanek <pavel.vlasanek@tucna.net>
2019-02-22 15:08:55 +0100

    Main file basic version

--- Raytracer/main.cpp
+++ Raytracer/main.cpp
@@ -5,5 +5,17 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     cout << "In progress..." << endl;
+
+    int width = 640;
+    int height = 480;
+
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+
+        }
+    }
+
     return 0;
 }
