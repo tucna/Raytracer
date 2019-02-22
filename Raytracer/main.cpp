d27788263b609bb2ec57e4ce692bb7aa5be52fa9
Pavel Vlasanek <pavel.vlasanek@tucna.net>
2019-02-22 16:31:52 +0100

    BMP save added

--- Raytracer/main.cpp
+++ Raytracer/main.cpp
@@ -1,6 +1,84 @@
+#define _CRT_SECURE_NO_DEPRECATE
+
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <limits>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
+
+struct RGBType {
+    double r;
+    double g;
+    double b;
+
+    RGBType(double rr, double gg, double bb) : r(rr), g(gg), b(bb) {}
+};
+
+void savebmp(const char *filename, int w, int h, int dpi, vector<RGBType> &data) {
+    FILE *f;
+    int k = w * h;
+    int s = 4 * k;
+    int filesize = 54 + s;
+
+    double factor = 39.375;
+    int m = static_cast<int>(factor);
+
+    int ppm = dpi * m;
+
+    unsigned char bmpfileheader[14] = { 'B','M', 0,0,0,0, 0,0,0,0, 54,0,0,0 };
+    unsigned char bmpinfoheader[40] = { 40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0,24,0 };
+
+    bmpfileheader[2] = (unsigned char)(filesize);
+    bmpfileheader[3] = (unsigned char)(filesize >> 8);
+    bmpfileheader[4] = (unsigned char)(filesize >> 16);
+    bmpfileheader[5] = (unsigned char)(filesize >> 24);
+
+    bmpinfoheader[4] = (unsigned char)(w);
+    bmpinfoheader[5] = (unsigned char)(w >> 8);
+    bmpinfoheader[6] = (unsigned char)(w >> 16);
+    bmpinfoheader[7] = (unsigned char)(w >> 24);
+
+    bmpinfoheader[8] = (unsigned char)(h);
+    bmpinfoheader[9] = (unsigned char)(h >> 8);
+    bmpinfoheader[10] = (unsigned char)(h >> 16);
+    bmpinfoheader[11] = (unsigned char)(h >> 24);
+
+    bmpinfoheader[21] = (unsigned char)(s);
+    bmpinfoheader[22] = (unsigned char)(s >> 8);
+    bmpinfoheader[23] = (unsigned char)(s >> 16);
+    bmpinfoheader[24] = (unsigned char)(s >> 24);
+
+    bmpinfoheader[25] = (unsigned char)(ppm);
+    bmpinfoheader[26] = (unsigned char)(ppm >> 8);
+    bmpinfoheader[27] = (unsigned char)(ppm >> 16);
+    bmpinfoheader[28] = (unsigned char)(ppm >> 24);
+
+    bmpinfoheader[29] = (unsigned char)(ppm);
+    bmpinfoheader[30] = (unsigned char)(ppm >> 8);
+    bmpinfoheader[31] = (unsigned char)(ppm >> 16);
+    bmpinfoheader[32] = (unsigned char)(ppm >> 24);
+
+    f = fopen(filename, "wb");
+
+    fwrite(bmpfileheader, 1, 14, f);
+    fwrite(bmpinfoheader, 1, 40, f);
+
+    for (int i = 0; i < k; i++) {
+        unsigned char color[3] = { round(data[i].r), round(data[i].g), round(data[i].b) };
+
+        fwrite(color, 1, 3, f);
+    }
+
+    fclose(f);
+}
 
 int main(int argc, char *argv[])
 {
@@ -8,14 +86,21 @@ int main(int argc, char *argv[])
 
     int width = 640;
     int height = 480;
+    int dpi = 72;
+
+    vector<RGBType> pixels;
 
     for (int x = 0; x < width; x++)
     {
         for (int y = 0; y < height; y++)
         {
+            int index = y * width + x;
 
+            pixels.push_back(RGBType(255.0, 0.0, 255.0));
         }
     }
+
+    savebmp("scene.bmp", width, height, dpi, pixels);
 
     return 0;
 }
